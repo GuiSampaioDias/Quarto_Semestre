6de3779079ef4f605d1caa2a4a36c52d7174954d
Guilherme Sampaio Dias <gui_sampaiodias@hotmail.com>
2024-04-20 00:23:54 -0300

    Uml

---
+++ MonitoriaPOO/aula6/medico.py
@@ -0,0 +1,34 @@
+class Paciente:
+    def __init__(self, nome, cpf, idade):
+        self.nome = nome
+        self.cpf = cpf
+        self.idade = idade
+
+class Medico:
+    def __init__(self, nome, crm, especializacao):
+        self.nome = nome
+        self.crm = crm
+        self.especializacao = especializacao 
+
+class Exame:
+    def __init__(self, medico, paciente, descricao, resultado):
+        self.medico = medico
+        self.paciente = paciente
+        self.descricao = descricao
+        self.resultado = resultado
+
+    def imprimir_exame(self):
+        resultado = f'''
+Paciente: {self.paciente.nome}   Médico: {self.medico.nome.rjust(15)}  
+CPF:      {self.paciente.cpf} CRM: {self.medico.crm.rjust(15)}
+'''
+        print(resultado)
+
+paciente = Paciente('Marcelo Silva', '033444555-22', 26)
+medico = Medico('Ana Beatriz', 333431, 'Clínico Geral')
+exame01 = Exame(medico, paciente, 'COVID-19', 'Negativo')
+exame01.imprimir_exame()
+# Deve exibir relatório com os dados do exame (inclusive os do médico e do paciente)
+
+
+  #imprimir_exame(): exibe um relatório com os dados do exame (inclusive os dadosdo médico e do paciente)      
