b1ba0b51139efd769d16877c8f09725d949bd2b5
Guilherme Sampaio Dias <gui_sampaiodias@hotmail.com>
2024-04-27 20:23:42 -0300

    React

---
+++ FullStack/my-app/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Welcome from './Welcome';
+import Contador from './Contador';
+import Clock from './Clock';
+
+
+// criando as duas variaveis que será usado no arquivo Welcome repare que dentro do root.render quando rederizamos o welcome nos colocamos as duas variáveis dentro do welcome 
+const name = "Guilherme";
+const data = new Date().toISOString();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     <Welcome name={name} data={data} />
+     <Contador />
+     <Clock />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
