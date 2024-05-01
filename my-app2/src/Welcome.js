b58cdf7ce08bf54df700e6e986c5012a3f1d732a
Guilherme Sampaio Dias <2202471@SALAS.AULAS>
2024-04-30 22:43:25 -0300

    Por favor funciona

---
+++ my-app2/src/Welcome.js
@@ -0,0 +1,4 @@
+function Welcome(props) {
+    return (<h1>Olá, {props.name} hoje é {props.data}</h1>);
+  }
+  export default Welcome;
---
+++ my-app2/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Welcome from './Welcome';
+import Contador from './Contador';
+import Clock from './Clock';
+import NameForm from './Form2';
+import Form from './Form3';
+
+const name = "Guilherme";
+const data = new Date().toISOString();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Welcome name = {name} data = {data} />
+    <Contador/>
+    <Clock/>
+    <NameForm/>
+    <Form/>
+  </React.StrictMode>
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
